4135b04800b331b072c842ccdd43b2e3f6495ed3
uu4k <t.shimizu1115@gmail.com>
2020-08-23 14:25:29 +0900

    add rust sample code(rust book ch06).

--- src/main.rs
+++ src/main.rs
@@ -144,6 +144,56 @@ fn main() {
         height: 50,
     };
     println!("Can rect2 hold rect3? {}", rect2.can_hold(&rect3));
+
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    let home2 = IpAddr2::V4(String::from("127.0.0.1"));
+    let loopback2 = IpAddr2::V6(String::from("::1"));
+
+    let home3 = IpAddr3::V4(127, 0, 0, 1);
+    let loopback3 = IpAddr3::V6(String::from("::1"));
+
+    // Option enuのSomeとNone
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+
+    let x: i8 = 8;
+    let y: Option<i8> = Some(5);
+    // yはNoneの可能性もあるのでOptionの場合はちゃんと判定入れる
+    // let sum = x + y;
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+
+    let some_u8_value = Some(0u8);
+    if let Some(3) = some_u8_value {
+        println!("three");
+    } else {
+        println!("default");
+    }
 }
 
 fn another_function(x: i32) {
@@ -207,8 +257,71 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn area2(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
+}
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+enum IpAddr2 {
+    V4(String),
+    V6(String),
+}
+
+enum IpAddr3 {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    // enumとmatchアーム
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
